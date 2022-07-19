f23eadb5422495d1e7dd40bbb9524090a1849710
hirayama-yuuichi <hirayama.yuuichi@gmail.com>
2022-07-20 06:51:03 +0900

    The version I entered as well as the model

--- src/towerofhanoi/__init__.py
+++ src/towerofhanoi/__init__.py
@@ -3,4 +3,4 @@ By hirayama yuuichi hirayama.yuuichi@gmail.com
 
 Build a game boat for the Tower of Hanoi"""
 
-__version__ = '0.1.0'
+__version__ = "0.1.0"
--- src/towerofhanoi/__main__.py
+++ src/towerofhanoi/__main__.py
@@ -1,4 +1,4 @@
 import towerofhanoi
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pass
